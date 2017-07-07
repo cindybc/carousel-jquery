95055eedd60daf43a06e3be867b2d5dcc3c21a52
cindy <cindialy@gmail.com>
2017-07-07 13:58:41 -0400

    jquery

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
+	//INTERVALO DE TIEMPO 5 SEG.
     $("#carousel").carousel({interval: 5000});
+
+    //PREV
+    $('.left').click(function() {
+    	$('#carousel').carousel("prev");
+    });
+    //NEXT
+    $('.right').click(function(){
+    	$('#carousel').carousel("next");
+    });
+
+    $('.item1').click(function(){
+    	$('#carousel').carousel(0);
+    });
+     $('.item2').click(function(){
+    	$('#carousel').carousel(1);
+    });
+      $('.item3').click(function(){
+    	$('#carousel').carousel(2);
+    });
+     $('.item4').click(function(){
+    	$('#carousel').carousel(3);
+    });
+     $('.item5').click(function(){
+    	$('#carousel').carousel(4);
+    });
+     $('.item6').click(function(){
+    	$('#carousel').carousel(5);
+    });  
 });
